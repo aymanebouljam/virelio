047dddb17a66afad3f9fca39a3928897d71ee6d8
Aymane Bouljam <235529830+aymanebouljam@users.noreply.github.com>
2026-08-30 16:13:51 +0100

    feat(auth): throttle recovery requests

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,12 +3,19 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { AuthMailService } from './auth-mail.service';
 
 @Module({
   imports: [
     ConfigModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60_000,
+        limit: 3,
+      },
+    ]),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
